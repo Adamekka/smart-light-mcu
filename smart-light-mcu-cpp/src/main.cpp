f752187a0d05699ff5cae5fd2d452a804f22adee
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-04 15:41:32 +0100

    get distance from `PIR` and print it to serial output

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
+#define PIR_PIN 19
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(PIR_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  auto distance = digitalRead(PIR_PIN);
+
+  Serial.println(distance);
+  delay(1000);
 }

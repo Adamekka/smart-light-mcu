402d85f4f761f102bde1ca2227990ed660a729bf
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-10 16:09:50 +0100

    rewrite using interrupt

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -1,8 +1,25 @@
 #include <Arduino.h>
 
+// Define the states
+enum LightState {
+  ON,
+  OFF
+} lightState;
+
 // Define the pins
 // All pins have interrupt capability
 #define PIR_PIN 19
+
+void pirInterrupt() {
+  // Check the state
+  if (lightState == ON) {
+    // Turn the light off
+    lightState = OFF;
+  } else {
+    // Turn the light on
+    lightState = ON;
+  }
+}
 
 void setup() {
   // Initialize serial
@@ -10,15 +27,21 @@ void setup() {
 
   // Set the pins
   pinMode(PIR_PIN, INPUT);
+
+  // Attach the interrupt
+  attachInterrupt(PIR_PIN, pirInterrupt, RISING);
 }
 
 void loop() {
-  // Read the distance
-  auto distance = digitalRead(PIR_PIN);
-
-  // Print the distance to the serial
-  Serial.println(distance);
-
-  // Wait for 1 second
-  delay(1000);
+  switch (lightState) {
+    case ON:
+      // Turn the light on
+      Serial.println("Light is on");
+      delay(500);
+      break;
+    case OFF:
+      // Turn the light off
+      Serial.println("Light is off");
+      break;
+  }
 }

87a369cabc5c92eb43238f340b77ebff0c7a2073
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-10 15:41:25 +0100

    add comments

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 
+// Define the pins
+// All pins have interrupt capability
 #define PIR_PIN 19
 
 void setup() {
+  // Initialize serial
   Serial.begin(115200);
 
+  // Set the pins
   pinMode(PIR_PIN, INPUT);
 }
 
 void loop() {
+  // Read the distance
   auto distance = digitalRead(PIR_PIN);
 
+  // Print the distance to the serial
   Serial.println(distance);
+
+  // Wait for 1 second
   delay(1000);
 }

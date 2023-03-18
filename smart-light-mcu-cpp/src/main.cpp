a270291ce20e78fb17ccc6b9a78d857f5f5abb7a
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-18 22:12:40 +0100

    cleanup

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -5,13 +5,12 @@
 
 // Configs
 #include "config.hpp"
-#include "wifi-config.hpp"
 
 // My libs
 #include "wifi.hpp"
 
 // You need to create your own `wifi-config.hpp` file
-// with your own WiFi credentials and Smart Light credentials
+// with your own WiFi credentials
 // See `wifi-config.hpp.example` for more info
 
 // Define the states
@@ -41,48 +40,12 @@ void pirInterrupt() {
   }
 }
 
-bool onPowerState(const String &deviceId, bool &state) {
-  Serial.printf("Device %s power turned %s \r\n", deviceId.c_str(), state?"on":"off");
-  Light.state = state?ON:OFF;
-  return true; // request handled properly
-}
-
-bool onBrightness(const String &deviceId, int &brightness) {
-  Light.brightness = brightness;
-  Serial.printf("Device %s brightness level changed to %d\r\n", deviceId.c_str(), brightness);
-  return true; // request handled properly
-}
-
-bool onAdjustBrightness(const String &deviceId, int brightnessDelta) {
-  Light.brightness += brightnessDelta;
-  Serial.printf("Device %s brightness level changed about %i to %d\r\n", deviceId.c_str(), brightnessDelta, Light.brightness);
-  brightnessDelta = Light.brightness;
-  return true; // request handled properly
-}
-
-void setupSinricPro() {
-  // get a new Light device from SinricPro
-  SinricProLight &myLight = SinricPro[LIGHT_ID];
-
-  // set callback function to device
-  myLight.onPowerState(onPowerState);
-  myLight.onBrightness(onBrightness);
-  myLight.onAdjustBrightness(onAdjustBrightness);
-
-  // setup SinricPro
-  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); });
-  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
-  SinricPro.begin(APP_KEY, APP_SECRET);
-}
-
 void setup() {
   // Initialize serial
   Serial.begin(115200);
 
   // Initialize WiFi
   setupWiFi();
-  // Initialize SinricPro
-  setupSinricPro();
 
   // Set the pins
   // Inputs

f3d1152058382d914da7629b4adb4a8029ac0924
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-19 14:23:52 +0100

    move `wifi.hpp` to `main.cpp`

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -1,12 +1,12 @@
 // Libs
 #include <Arduino.h>
 #include <HTTPClient.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 
 // Configs
 #include "config.hpp"
-
-// My libs
-#include "wifi.hpp"
+#include "wifi-config.hpp"
 
 // You need to create your own `wifi-config.hpp` file
 // with your own WiFi credentials
@@ -84,6 +84,30 @@ public:
     smartLightOff();
   }
 } Light;
+
+auto setupWiFi() -> void {
+  Serial.printf("\r\n[Wifi]: Connecting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  for (int i = 0; i < 60; i++) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.printf(".");
+      delay(250);
+    } else {
+      break;
+    }
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.printf("\r[WiFi]: Failed to connect to WiFi\n");
+    Serial.println("[WiFi]: Smart light will not work without WiFi connection");
+  } else {
+    IPAddress localIP = WiFi.localIP();
+    Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n",
+                  localIP[0], localIP[1], localIP[2], localIP[3]);
+  }
+}
 
 auto pirInterrupt() -> void {
   // Check the state

bdff2f5cad6d9423a8b5f3e0ac3a8b337e88f832
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-19 14:15:09 +0100

    handle no WiFi connection

--- smart-light-mcu-cpp/src/wifi.hpp
+++ smart-light-mcu-cpp/src/wifi.hpp
@@ -6,17 +6,26 @@
 // Configs
 #include "wifi-config.hpp"
 
-void setupWiFi() {
+auto setupWiFi() -> void {
   Serial.printf("\r\n[Wifi]: Connecting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.printf(".");
-    delay(250);
+  for (int i = 0; i < 60; i++) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.printf(".");
+      delay(250);
+    } else {
+      break;
+    }
   }
 
-  IPAddress localIP = WiFi.localIP();
-  Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n",
-                localIP[0], localIP[1], localIP[2], localIP[3]);
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.printf("\r[WiFi]: Failed to connect to WiFi\n");
+    Serial.println("[WiFi]: Smart light will not work without WiFi connection");
+  } else {
+    IPAddress localIP = WiFi.localIP();
+    Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n",
+                  localIP[0], localIP[1], localIP[2], localIP[3]);
+  }
 }

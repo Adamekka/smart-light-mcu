299f29e1a6c6cda90f059ae109cbc813390e0c39
Adam Cvikl <adam.wasd4@gmail.com>
2023-03-21 20:05:37 +0100

    add comments

--- smart-light-mcu-cpp/src/main.cpp
+++ smart-light-mcu-cpp/src/main.cpp
@@ -24,7 +24,7 @@ HTTPClient http;
 // Define a class for the light
 class Light {
 private:
-  // Define a private function to handle HTTP results
+  // Define a private method to handle HTTP results
   auto handleHttpResult(int httpCode) -> void {
     if (httpCode < 0) {
       // If the HTTP request failed, print an error message
@@ -42,7 +42,7 @@ private:
     }
   }
 
-  // Define a private function to turn on the smart light
+  // Define a method function to turn on the smart light
   auto smartLightOn() -> void {
     // Send the request to turn on the light
     Serial.println("[HTTP] begin...");
@@ -54,7 +54,7 @@ private:
     http.end();
   }
 
-  // Define a private function to turn off the smart light
+  // Define a private method to turn off the smart light
   auto smartLightOff() -> void {
     // Send the request to turn off the light
     Serial.println("[HTTP] begin...");
@@ -73,9 +73,14 @@ public:
     OFF,
   } state = OFF;
 
+  // `activated` is used to break the loop
+  // after the first iteration of the loop
+  // when PIR value changes
+  // This is needed because the loop would
+  // send multiple requests to the smart light
   bool activated = false;
 
-  // Define a public function to turn the light on
+  // Define a public method to turn the light on
   auto on() -> void {
     // Turn on the LED
     digitalWrite(LED_PIN, HIGH);
@@ -83,7 +88,7 @@ public:
     smartLightOn();
   }
 
-  // Define a public function to turn the light off
+  // Define a public method to turn the light off
   auto off() -> void {
     // Turn off the LED
     digitalWrite(LED_PIN, LOW);
@@ -103,6 +108,10 @@ auto setupWiFi() -> void {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   // Wait for the connection to be established
+  // If the connection is not established after 15 seconds,
+  // print an error message and continue without WiFi
+  // Smart light will not work without WiFi connection
+  // If the connection is established, print the IP address
   for (int i = 0; i < 60; i++) {
     if (WiFi.status() != WL_CONNECTED) {
       Serial.printf(".");
@@ -123,15 +132,22 @@ auto setupWiFi() -> void {
   }
 }
 
+// This function is called when the PIR sensor value changes
+// If the PIR sensor is HIGH, turn the light on
+// If the PIR sensor is LOW, turn the light off
 auto pirInterrupt() -> void {
-  // Check the state
+  // Check the state of the PIR sensor
+  // then switch the light state to the opposite
+  // and set `activated` to false
   switch (light.state) {
   case Light::State::ON:
+    // Set `activated` to false
     light.activated = false;
     // Turn the light off
     light.state = Light::State::OFF;
     break;
   case Light::State::OFF:
+    // Set `activated` to false
     light.activated = false;
     // Turn the light on
     light.state = Light::State::ON;
@@ -139,8 +155,8 @@ auto pirInterrupt() -> void {
   }
 }
 
+// The setup function is called once at startup
 auto setup() -> void {
-
   // Initialize serial
   Serial.begin(115200);
 
@@ -149,9 +165,9 @@ auto setup() -> void {
 
   // Set the pins
   // Inputs
-  pinMode(PIR_PIN, INPUT);
+  pinMode(PIR_PIN, INPUT); // PIR sensor
   // Outputs
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); // LED
 
   // Attach the interrupt
   attachInterrupt(PIR_PIN, pirInterrupt, CHANGE);
@@ -159,11 +175,21 @@ auto setup() -> void {
   Serial.println("Setup done!");
 }
 
+// The loop function is called in an endless loop
 auto loop() -> void {
+  // Check if the light is activated
+  // If it already has been, break the loop
+  // This is needed because the loop would
+  // send multiple requests to the smart light
   if (light.activated) {
     // Break loop
     return;
   }
+
+  // Check the state of the `light.state` value that has been set at the
+  // interrupt and turn it on or off accordingly then set `activated` to true so
+  // that the loop is not executed again because we don't want to send multiple
+  // requests to the smart light
   switch (light.state) {
   case Light::State::ON:
     // Turn the light on
